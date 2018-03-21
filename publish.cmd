c44ff18926ef1a6374e1b5bda30356026ed285fa
Andree Ochoa <andlody@gmail.com>
2018-03-21 02:01:48 -0500

    Agregar archivos de proyecto.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=tiendabots-Web-Deploy.pubxml -p:Password=K15leBTT14QpaNSbos4w9Lc4XZwPKntHpoYwfe974l24JbNg1xLRMv0Q5wnK
+
